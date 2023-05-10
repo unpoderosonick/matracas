12a92b109f198f838063b50d28d9967f42267fd9
Javier Gutierrez <j_gutierrez-itind@kw.com>
2023-05-10 18:33:20 -0500

    perform dashboard extras utils

--- cesem/dashboard/templatetags/dashboard_extras.py
+++ cesem/dashboard/templatetags/dashboard_extras.py
@@ -4,11 +4,17 @@ from django import template
 register = template.Library()
 
 
-def get_columns_names(value, arg):
+def get_columns_labels(view_model, colums):
     columns = []
-    for column in arg:
-        columns.append(value.get_serializer().fields[column].label)
+    for column in colums:
+        columns.append(view_model.get_serializer().fields[column].label)
     return columns
+
+
+def get_column_label(view_model, column_name):
+    if column_name in view_model.get_serializer():
+        return view_model.get_serializer().fields[column_name].label
+    return column_name
 
 
 def get_home_path(value):
@@ -41,7 +47,8 @@ def get_start_date_of_week(week_number, year):
     return start_of_week
 
 
-register.filter("get_columns_names", get_columns_names)
+register.filter("get_columns_labels", get_columns_labels)
+register.filter("get_column_label", get_column_label)
 register.filter("get_model_name_plural", get_model_name_plural)
 register.filter("get_model_name", get_model_name)
 register.filter("get_home_path", get_home_path)
