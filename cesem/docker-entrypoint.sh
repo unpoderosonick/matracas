75d9325cee64576ad30ce05ac8da0e08d0d0a714
Javier Gutierrez <j_gutierrez-itind@kw.com>
2023-05-11 18:59:33 -0500

    fixed docker-compose

--- cesem/docker-entrypoint.sh
+++ cesem/docker-entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-echo "Migrate the Database at startup of project"
-
-# Wait for few minute and run db migraiton
-while ! python manage.py migrate  2>&1; do
-   echo "Migration is in progress status"
-   sleep 3
-done
-
-echo "Django docker is fully configured successfully."
+python /code/manage.py collectstatic --noinput
+python manage.py migrate --noinput
+exec python manage.py runserver 0.0.0.0:8000
 
 exec "$@"
