d326848afd412c25b34d9015e1026ab4c157ced3
Javier Gutierrez <j_gutierrez-itind@kw.com>
2023-05-13 00:59:43 -0500

    set docker-compose runserver for gunicorn

--- cesem/entrypoint.sh
+++ cesem/entrypoint.sh
@@ -13,5 +13,5 @@ echo "-------------------------Apply loaddata migrations"
 python manage.py loaddata users.json
 
 # Start server
-echo "-------------------------Starting server"
-python manage.py runserver 0.0.0.0:8000
+# echo "-------------------------Starting server"
+# python manage.py runserver 0.0.0.0:8000
